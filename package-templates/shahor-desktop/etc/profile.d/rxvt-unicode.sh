110904a538413532048c35f940dce1771fc23a6c
Dima Krasner <dima@dimakrasner.com>
2013-06-01 16:20:55 +0000

    Added a file dropped from the previous commit.

---
+++ package-templates/shahor-desktop/etc/profile.d/rxvt-unicode.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# /etc/profile.d/rxvt-unicode.sh: environmental variables related to
+#                                 rxvt-unicode
+
+# set the urxvtd socket path
+export RXVT_SOCKET="$HOME/.urxvt_socket"
