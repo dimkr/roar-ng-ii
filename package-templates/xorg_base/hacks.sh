94b6f09129a86853c9e1e56fa05530dd6328c3dd
Dima Krasner <dima@dimakrasner.com>
2013-06-22 08:29:38 +0000

    Bug fix - if rgb.txt exists already, do not create a symlink.

--- package-templates/xorg_base/hacks.sh
+++ package-templates/xorg_base/hacks.sh
@@ -4,8 +4,16 @@
 rm -f ./etc/X11/xinit/xinitrc
 
 # create a /usr/share/X11/rgb.txt, a symlink to rgb.txt
-[ ! -e ./usr/share/X11 ] && mkdir ./usr/share/X11
-ln -s $(find . -name rgb.txt -type f | cut -b 2-) ./usr/share/X11/rgb.txt
+if [ ! -e ./usr/share/X11 ]
+then
+	mkdir ./usr/share/X11
+else
+	if [ ! -e ./usr/share/X11/rgb.txt ]
+	then
+		ln -s "$(find . -name rgb.txt -type f | cut -b 2-)" \
+		      ./usr/share/X11/rgb.txt
+	fi
+fi
 
 # disable bitmap fonts
 [ -e ./etc/fonts/conf.avail/70-no-bitmaps.conf ] && \
